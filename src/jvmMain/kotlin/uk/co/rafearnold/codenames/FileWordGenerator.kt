176cd82bafe4785b577a217b5a409a7d61a5f772
Rafe Arnold <rafe.arnold@gmail.com>
2021-08-07 22:07:35 +0100

    Update FileWordGenerator to resolve file using vertx.

--- src/jvmMain/kotlin/uk/co/rafearnold/codenames/FileWordGenerator.kt
+++ src/jvmMain/kotlin/uk/co/rafearnold/codenames/FileWordGenerator.kt
@@ -1,19 +1,20 @@
 package uk.co.rafearnold.codenames
 
+import io.vertx.core.Vertx
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import java.io.File
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
 class FileWordGenerator @Inject constructor(
-    @Named("words-file") private val wordsFile: String
+    @Named("words-file") wordsFile: String,
+    vertx: Vertx
 ) : WordGenerator {
 
     private val words: List<String> =
-        Json.decodeFromString(File(wordsFile).readText())
+        Json.decodeFromString(vertx.fileSystem().readFileBlocking(wordsFile).toString())
 
     override fun getWords(count: Int): List<String> =
         words.shuffled().take(count)
